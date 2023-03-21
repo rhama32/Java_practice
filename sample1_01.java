d494dc36fe43cccc7859f027d01cd7f12db2bd44
rhama32 <r.hamaoka32915@gmail.com>
2023-03-21 11:50:10 +0000

    小数点の学習

--- sample1_01.java
+++ sample1_01.java
@@ -1,6 +1,15 @@
 package com.example.mynavi.basic;
  
-public class Variable {
+// public class Variable {
+//   public static void main(String[] args) {
+//     double num = 3.14;
+//     System.out.println(num);
+
+class Main {
   public static void main(String[] args) {
-    double num = 3.14;
-    System.out.println(num);
+    double number1 = 11.5;
+    double number2 = 11.2;
+    System.out.println(number1 + number2);
+    System.out.println(number1 - number2);
+  }
+}
