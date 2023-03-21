1af7b0702beb97e450912ab2751c25b5a7fb5080
rhama32 <r.hamaoka32915@gmail.com>
2023-03-21 11:57:47 +0000

    autmatic型変換

--- practice.java
+++ practice.java
@@ -5,11 +5,26 @@ package com.example.mynavi.basic;
 //     double num = 3.14;
 //     System.out.println(num);
 
+//小数点学習
 class Main {
   public static void main(String[] args) {
     double number1 = 11.5;
     double number2 = 11.2;
     System.out.println(number1 + number2);
     System.out.println(number1 - number2);
+  }
+}
+
+//自己連結
+class Main {
+  public static void main(String[] args) {
+    int month = 2;
+    int date = 1;
+    
+    // 「2月1日」
+    System.out.println( month + "月" + date + "日");
+    System.out.println(2/1);
+    System.out.println(2.0/1.0);
+    System.out.println(2.0/1);
   }
 }
