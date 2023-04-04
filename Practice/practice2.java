7dd034b9d6211af3b8c2d3580efdad7ec25b4d76
rhama32 <r.hamaoka32915@gmail.com>
2023-04-04 10:55:46 +0000

    Java0404

--- Practice/practice2.java
+++ Practice/practice2.java
@@ -1,5 +1,5 @@
 class Program01 {
 	public static void main(String[] args) {
-	  System.out.println("Mike");
+	  System.out.println("Hama");
 	}
 }
--- Practice/practice3.java
+++ Practice/practice3.java
@@ -5,7 +5,7 @@
 class practice3 {
 	public static void main (String[] args) {
 		int	teika   = 1000 ;             //定価の金額
-		double zei = 1.10 ;             //消費税
+		double zei = 1.20 ;             //消費税
 		double gokei = teika * zei ;  //支払い額を計算
 		System.out.println(gokei) ;       //支払い額を画面に表示
 	}
