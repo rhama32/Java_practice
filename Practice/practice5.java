e0bea0962d8e53a5ef5b9477cbc54eddef0a8339
rhama32 <r.hamaoka32915@gmail.com>
2023-04-05 13:45:46 +0000

    java

---
+++ Practice/practice5.java
@@ -0,0 +1,12 @@
+class practice5 {
+  public static void main (String[] args) {
+  
+    int Salery = 1000;
+    
+    char word = '挑';
+    
+    String job = "Engineer";
+    
+    boolean graduate = false;
+  }
+}
