9086046a6cb649a6773ed78d425770438371480d
rhama32 <r.hamaoka32915@gmail.com>
2023-04-05 13:17:48 +0000

    変数

--- Practice/practice3.java
+++ Practice/practice3.java
@@ -5,8 +5,15 @@
 class practice3 {
 	public static void main (String[] args) {
 		int	teika   = 1000 ;             //定価の金額
-		double zei = 1.20 ;             //消費税
+		double zei = 1.10 ;             //消費税
 		double gokei = teika * zei ;  //支払い額を計算
 		System.out.println(gokei) ;       //支払い額を画面に表示
 	}
 }
+
+// intは整数専用の変数
+//箱の中にはデータは一つのみ
+//teikaの箱に1000（値）を代入
+//変数を新しく作ること＝変数宣言
+//_ スネークケース
+//list_Gokei キャメルケース
