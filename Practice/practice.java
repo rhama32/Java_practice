b36ec5bc3fb0a849df19d2389f6c683cdf7c1c8c
濱岡良維 <117173971+rhama32@users.noreply.github.com>
2023-05-12 11:08:10 +0900

    Update practice.java

--- Practice/practice.java
+++ Practice/practice.java
@@ -9,6 +9,26 @@ class Main {
   }
 }
 
+// class Main {
+//   public static void main(String[] args) {
+//     // 変数numbersに、与えられた数字の配列を代入
+//     int [] numbers = {1, 4, 6, 9, 13, 16};
+    
+//     int oddSum = 0;
+//     int evenSum = 0;
+    
+//     // for文を用いて、配列numbersの偶数の和と奇数の和
+//     for (int number : numbers) {
+//       if (number % 2 == 0) {
+//         evenSum += number;
+//       } else {
+//         oddSum += number;
+//       }
+//     }
+//     System.out.println("奇数の和は" + oddSum );
+//     System.out.println("偶数の和は" + evenSum );
+//   }
+// }
 
 // class Main {
 //   public static void main(String[] args) {
