3a942e76080e6e564fb223331d4de5e007d9559b
濱岡良維 <117173971+rhama32@users.noreply.github.com>
2023-05-16 20:15:18 +0900

    Update practice2.java

--- Practice/practice2.java
+++ Practice/practice2.java
@@ -1,3 +1,10 @@
+
+class Program01 {
+	public static void main(String[] args) {
+	  System.out.println("Hama");
+	}
+}
+
 class Program01 {
 	public static void main(String[] args) {
 	  System.out.println("Hama");
